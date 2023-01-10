4b944328028b7216b3c8b18262d5ad0ad3f634d1
Felipe ALCM <felipealcmagalhaes@gmail.com>
2023-01-10 16:21:20 -0300

    Tentando melhorar a OO - Divisão de classes e tarefas

---
+++ JogoDaVelhaV1/Models/Board.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaVelhaV1.Models
+{
+    public class Board
+    {
+        private static char[] positions = 
+            { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        public Board()
+        {
+
+        }
+
+        public static void ShowBoard()
+        {
+            Console.Clear();
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"|{positions[j]}|");
+            }
+            Console.WriteLine();
+            for (int j = 3; j < 6; j++)
+            {
+                Console.Write($"|{positions[j]}|");
+            }
+            Console.WriteLine();
+            for (int j = 6; j < 9; j++)
+            {
+                Console.Write($"|{positions[j]}|");
+            }
+            Console.WriteLine();
+        }
+
+        public static bool PlacePiece(char positionChosed, char vez)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                if (positions[i] == positionChosed)
+                {
+                    positions[i] = vez;
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Posição escolhida já foi ocupada ou é inválida");
+                    return false;
+                }              
+            }
+            Console.WriteLine("Posição Inválida");
+            return false;
+        }
+
+    }
+}
--- JogoDaVelhaV1/Program.cs
+++ JogoDaVelhaV1/Program.cs
@@ -1,4 +1,6 @@
-﻿using JogoDaVelhaV1.Sistemas;
+﻿using JogoDaVelhaV1.Models;
+using JogoDaVelhaV1.Sistemas;
+using JogoDaVelhaV1.Views;
 using System;
 
 namespace JogoDaVelhaV1
@@ -7,8 +9,25 @@ namespace JogoDaVelhaV1
     {
         public static void Main(string[] args)
         {
-            Sistema.IniciarSistema();
+            int option = 10;
+            Menus.ShowWelcome();
 
+            while (option != 0)
+            {
+                Menus.ShowOptions();
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Board.ShowBoard();
+                        Console.WriteLine("Aperte ENTER para continuar");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+            }
         }
     }
 }
